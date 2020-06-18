25fd465050151e7fcb030002e1e1cce6246838b2
Arielle Domantay <domantayarielle@gmail.com>
2020-06-17 21:05:26 -0400

    created the handle functions WIP find and replace function

--- client/components/Edit.js
+++ client/components/Edit.js
@@ -7,16 +7,19 @@ import Typewriter from 'typewriter-effect'
  */
 
 const initialState = {
-  title: '',
   body: '',
-  phrases: []
+  phrases: [],
+  newCover: ''
 }
 export class Edit extends React.Component {
   constructor() {
     super()
     this.state = initialState
     this.handleChange = this.handleChange.bind(this)
-    // this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
+    this.findAndReplace = this.findAndReplace.bind(this)
   }
 
   handleChange() {
@@ -25,28 +28,86 @@ export class Edit extends React.Component {
     })
   }
 
-  handleChange() {
-    event.preventDefault()
+  findAndReplace(text, find, replace) {
+    text.replace(find, replace)
   }
+
+  handleAdd(event) {
+    let phrases = this.state.phrases
+    phrases = [...phrases, ['', '']]
+    this.setState({phrases})
+  }
+
+  handleDelete(event) {
+    let phrases = this.state.phrases
+    let index = Number(event.target.name)
+
+    phrases.splice(index, 1)
+
+    this.setState({
+      phrases
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    let letter
+    this.state.phrases.map(phrase => {
+      letter = this.state.body.replace(phrase[0], phrase[1])
+    })
+
+    console.log(letter)
+
+    this.setState({
+      newCover: letter
+    })
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div id="edit">
-        <button id="add-bttn" type="button">
+        <button id="add-bttn" type="button" onClick={this.handleAdd}>
           Add
         </button>
-        <div id="phrases">
-          <input id="find" type="text" onChange={this.handleChange} />
-          <input id="replace" type="text" onChange={this.handleChange} />
-        </div>
+        {this.state.phrases.map(([find, replace], index) => {
+          const changeFind = event => {
+            let phrases = this.state.phrases
+            phrases[index][0] = event.target.value // FIXME?
+            this.setState({phrases})
+          }
+          const changeReplace = event => {
+            let phrases = this.state.phrases
+            phrases[index][1] = event.target.value // FIXME?
+            this.setState({phrases})
+          }
+          return (
+            <div key={index}>
+              <button
+                name={index}
+                id="delete-btn"
+                type="button"
+                onClick={this.handleDelete}
+              >
+                Delete
+              </button>
+              <input type="text" value={find} onChange={changeFind} />
+              <input type="text" value={replace} onChange={changeReplace} />
+            </div>
+          )
+        })}
         <input
           name="body"
           id="letter"
           type="text"
           onChange={this.handleChange}
         />
-        <button id="submit-bttn" type="button">
+        <button id="submit-bttn" type="button" onClick={this.handleSubmit}>
           Submit
         </button>
+
+        <div id="new-cover">{this.state.newCover}</div>
       </div>
     )
   }
